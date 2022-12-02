313d4d0b79135fd68cdb749b0fb731dfcb4e607e
Andrey Dubinchik <evil_ghost@bk.ru>
2022-12-02 13:41:56 -0800

    update project (#8)
    * update project
    
    * remove comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
-import os
 import sys
+import os
+
 
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(root_dir)
@@ -7,22 +8,23 @@ sys.path.append(root_dir)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 root_dir_content = os.listdir(BASE_DIR)
 PROJECT_DIR_NAME = 'yatube_api'
-# проверяем, что в корне репозитория лежит папка с проектом
+
 if (
         PROJECT_DIR_NAME not in root_dir_content
         or not os.path.isdir(os.path.join(BASE_DIR, PROJECT_DIR_NAME))
 ):
-    assert False, (
-        f'В директории `{BASE_DIR}` не найдена папка c проектом `{PROJECT_DIR_NAME}`. '
-        f'Убедитесь, что у вас верная структура проекта.'
+    raise AssertionError(
+        f'В директории `{BASE_DIR}` не найдена папка c проектом '
+        f'`{PROJECT_DIR_NAME}`. Убедитесь, что у вас верная структура '
+        'проекта.'
     )
 
 MANAGE_PATH = os.path.join(BASE_DIR, PROJECT_DIR_NAME)
 project_dir_content = os.listdir(MANAGE_PATH)
 FILENAME = 'manage.py'
-# проверяем, что структура проекта верная, и manage.py на месте
+
 if FILENAME not in project_dir_content:
-    assert False, (
+    raise AssertionError(
         f'В директории `{MANAGE_PATH}` не найден файл `{FILENAME}`. '
         f'Убедитесь, что у вас верная структура проекта.'
     )
